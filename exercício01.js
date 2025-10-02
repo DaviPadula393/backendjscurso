af2781996f77a218b03bb0489b5b8c111b6ae6e7
DaviPadula393 <davipadula86@gmail.com>
2025-10-01 22:04:07 -0300

    exercício da aula 01 do js

---
+++ exercício01.js
@@ -0,0 +1,9 @@
+const nome = "Davi";
+const sobrenome = "Padula";
+const idade = "17";
+const peso = "56.6";
+const altura = "1.79";
+
+const imc = peso / (altura * altura);
+
+console.log(nome+" tem "+idade+" anos, pesa "+peso+", tem "+altura+"altura e seu IMC é de "+imc)
---
+++ indev.js
@@ -0,0 +1,8 @@
+console.log("ola mundo!");
+let nome = "Davi";
+const sobrenome = "Padula"
+console.log(nome, sobrenome);
+// const significa que a variável não pode ser alterado caso tente, dará erro
+// let significa que a variável pode ser alterada
+// meu primeiro código
+// o javascript lê de cima pra baixo da esquerda pra direita, da msm de ler um livro
